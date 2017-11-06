3b1dd052539f22b14bc6bad6629e5828a976f69a
maxim.r <maxim.r@fora-soft.com>
2017-11-07 00:08:01 +0300

    Derivative loss introduced

--- test31.py
+++ test31.py
@@ -15,13 +15,20 @@ def f(x1, x2):
     return theta[0] + theta[1] * x1 + theta[2] * x2
 
 
-def deriv_loss():
+def deriv_loss(theta_index):
     sum = 0
     for i in data:
-        sum += abs(data[Y] - f(data[X1], data[X2]))
+        if theta_index == 0:
+            sum += abs(i[Y] - f(i[X1], i[X2]))
+        if theta_index == 1:
+            sum += abs(i[Y] - f(i[X1], i[X2])) * X1
+        if theta_index == 2:
+            sum += abs(i[Y] - f(i[X1], i[X2])) * X2
     sum *= 2
+    return sum
 
 
 for i in range(epochs):
     for j in range(len(theta)):
-        j = j - alpha * deriv_loss()
+        theta[j] -= alpha * deriv_loss(j)
+
