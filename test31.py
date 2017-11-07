171150b1175e54706cbe2c682e7a6e4203bca98b
maxim.r <maxim.r@fora-soft.com>
2017-11-07 15:13:05 +0300

    Pretty results

--- test31.py
+++ test31.py
@@ -1,4 +1,4 @@
-from text_utils import read_data
+from text_utils import read_data, process_input
 
 Y = 2
 X1 = 0
@@ -6,39 +6,85 @@ X2 = 1
 
 # init
 alpha = 0.1
-epochs = 25
+eps = 0.0000001
+epochs = 250
 data = read_data()
 theta = [0, 0, 0]
+
+y_max = 0
+x1_max = 0
+x2_max = 0
 
 
 def f(x1, x2):
     return theta[0] + theta[1] * x1 + theta[2] * x2
 
 
+def normalize(X):
+    # normilize X table
+    global y_max, x1_max, x2_max
+    for i in data:
+        if i[Y] > y_max:
+            y_max = i[Y]
+    for i in range(len(data)):
+        data[i][Y] /= y_max
+    for i in data:
+        if i[X1] > x1_max:
+            x1_max = i[X1]
+    for i in range(len(data)):
+        data[i][X1] /= x1_max
+    for i in data:
+        if i[X2] > x2_max:
+            x2_max = i[X2]
+    for i in range(len(data)):
+        data[i][X2] /= x2_max
+
+    return X
+
+
 def deriv_loss(theta_index):
     sum = 0
-    for i in data:
+    for data_row in data:
         if theta_index == 0:
-            sum += abs(i[Y] - f(i[X1], i[X2]))
-        if theta_index == 1:
-            sum += abs(i[Y] - f(i[X1], i[X2])) * X1
-        if theta_index == 2:
-            sum += abs(i[Y] - f(i[X1], i[X2])) * X2
-    sum *= 2
+            sum += (data_row[Y] - f(data_row[X1], data_row[X2]))
+        elif theta_index == 1:
+            sum += (data_row[Y] - f(data_row[X1], data_row[X2])) * data_row[X1]
+        elif theta_index == 2:
+            sum += (data_row[Y] - f(data_row[X1], data_row[X2])) * data_row[X2]
+    sum *= 2 / len(data)
     return sum
 
-
-for i in range(epochs):
+data = normalize(data)
+# for i in range(epochs):
+j_ = 100
+while abs(j_) > eps:
     for j in range(len(theta)):
-        theta[j] -= alpha * deriv_loss(j)
+        j_ = alpha * deriv_loss(j)
+        theta[j] += j_
 
 aver = 0
 err = []
+abs_err = []
 for i in data:
     x_ = i[Y] - f(i[X1], i[X2])
-    err.append(x_)
-    aver += x_
-    print(x_)
+    print(x_ * y_max)
+    abs_err.append(x_)
+    err.append(x_ ** 2)
+    aver += i[Y]
+print()
 aver /= len(data)
+for i in err:
+    print(i / aver)
 print()
 print(aver)
+print(theta)
+theta[0] *= y_max
+theta[1] *= x1_max
+theta[2] *= x2_max
+print(theta)
+while True:
+    s = input("New point")
+    ints = process_input(s, 2)
+    print(f(ints[0], ints[1]))
+
+
--- text_utils.py
+++ text_utils.py
@@ -1,12 +1,14 @@
-
 def read_data():
     with open("in.txt", "r") as f:
         res = []
         for i in f.readlines():
-            words = i.split(",")
-            nums = [int(words[0]), int(words[1]), int(words[2])]
-            res.append(nums)
+            res.append(process_input(i))
         return res
 
 
-
+def process_input(input, k=3):
+    words = input.split(",")
+    nums = []
+    for i in range(k):
+        nums.append(int(words[i]))
+    return nums
