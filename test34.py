8620a09fa71451cb64980db7866d343cc865683f
Maxim.r <maxim.r@fora-soft.com>
2017-11-20 14:31:26 +0300

    Changing random to sorted crossover

--- test34.py
+++ test34.py
@@ -5,6 +5,8 @@ import numpy as np
 from text_utils import read_data, process_input, read_data_np
 from standrad_utils import standardize, destandardize_value
 
+RAND_CONST = 50000
+
 data = read_data_np()
 x, y = data[..., 0:3], data[..., 3]
 n = len(x)
@@ -17,19 +19,29 @@ delta_stop_lim = 0.00000001
 
 # def f(x1, x2):
 # return theta[0] + theta[1] * x1 + theta[2] * x2
+def random_num():
+    return np.random.rand() * RAND_CONST
+
+
+def random_arr():
+    return np.random.rand(3) * RAND_CONST
 
 
 def init_population():
     global population
     for i in range(population_start_size - 1):
-        population = np.vstack([population, np.random.rand(3) * 10])
+        population = np.vstack([population, random_arr()])
         # population.append([random.randint(1000)/1000, random.randint(10000)/1000, random.randint(2000)/1000])
     pass
 
 
 def crossover(father_index, mother_index):
-    father = population[father_index]
-    mother = population[mother_index]
+    if bool(random.getrandbits(1)):
+        father = population[father_index]
+        mother = population[mother_index]
+    else:
+        father = population[mother_index]
+        mother = population[father_index]
     separator_index = random.getrandbits(1) + 1
     new_guy = father[:separator_index]
     new_guy = np.append(new_guy, mother[separator_index:])
@@ -40,7 +52,7 @@ def mutate(new_solution):
     is_mutating = np.random.choice([True, False], 1, p=[mutate_probability, 1 - mutate_probability])
     if is_mutating:
         index = random.randint(1, 3) - 1
-        new_solution[index] = random.randint(1, 20000) / 1000 - 10
+        new_solution[index] = random_num()
     return new_solution
 
 
@@ -75,12 +87,14 @@ while should_continue():
     survival_chance = reverse_fitness / coeff_sum
     new_population = np.array([])
     for i in range(population_start_size):
-        choice = np.random.choice(np.arange(len(survival_chance)), 2, p=survival_chance)
+        choice = [0, 0]
+        while choice[0] == choice[1]:
+            choice = np.random.choice(np.arange(len(survival_chance)), 2, p=survival_chance)
         new_solution = crossover(choice[0], choice[1])
         new_solution = mutate(new_solution)
         if len(new_population) == 0:
             new_population = new_solution
         else:
             new_population = np.vstack([new_population, new_solution])
-    # print(new_solution)
+            # print(new_solution)
 print(population)
